24e1990782403cb306d8e74e31537cf7862ed24a
Dmitri Shuralyov <shurcooL@gmail.com>
2016-09-05 00:41:06 -0700

    Add Component and RenderComponents{,String}.
    These are somewhat experimental and may change.
    
    The motivation is to be able to define higher level components, and
    render them conveniently.

--- htmlg.go
+++ htmlg.go
@@ -4,7 +4,9 @@ package htmlg
 
 import (
 	"bytes"
+	"fmt"
 	"html/template"
+	"io"
 
 	"golang.org/x/net/html"
 	"golang.org/x/net/html/atom"
@@ -36,8 +38,10 @@ func A(s string, href template.URL) *html.Node {
 	return a
 }
 
-// Render renders HTML nodes.
+// Render renders HTML nodes, returning result as template.HTML.
 // Context-aware escaping is done just like in html/template when rendering nodes.
+//
+// TODO: Return string instead of template.HTML; returning template.HTML has proven to be unhelpful (since so many consumers expect a simple string).
 func Render(nodes ...*html.Node) template.HTML {
 	var buf bytes.Buffer
 	for _, node := range nodes {
@@ -46,8 +50,45 @@ func Render(nodes ...*html.Node) template.HTML {
 			// html.Render should only return a non-nil error if there's a problem writing to the supplied io.Writer.
 			// We don't expect that to ever be the case (unless there's not enough memory), so panic.
 			// If this ever happens in other situations, it's a bug in this library that should be reported and fixed.
-			panic(err)
+			panic(fmt.Errorf("internal error: html.Render returned non-nil error, this is not expected to happen: %v", err))
 		}
 	}
 	return template.HTML(buf.String())
+}
+
+// Component is anything that can render itself into HTML nodes.
+type Component interface {
+	Render() []*html.Node
+}
+
+// RenderComponents renders components into HTML, writing result to w.
+// Context-aware escaping is done just like in html/template when rendering nodes.
+func RenderComponents(w io.Writer, components ...Component) error {
+	for _, c := range components {
+		for _, node := range c.Render() {
+			err := html.Render(w, node)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// RenderComponentsString renders components into HTML, returning result as a string.
+// Context-aware escaping is done just like in html/template when rendering nodes.
+func RenderComponentsString(components ...Component) string {
+	var buf bytes.Buffer
+	for _, c := range components {
+		for _, node := range c.Render() {
+			err := html.Render(&buf, node)
+			if err != nil {
+				// html.Render should only return a non-nil error if there's a problem writing to the supplied io.Writer.
+				// We don't expect that to ever be the case (unless there's not enough memory), so panic.
+				// If this ever happens in other situations, it's a bug in this library that should be reported and fixed.
+				panic(fmt.Errorf("internal error: html.Render returned non-nil error, this is not expected to happen: %v", err))
+			}
+		}
+	}
+	return buf.String()
 }
