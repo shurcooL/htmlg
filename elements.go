d697e68c200a67a3d35a1b6650d9c18533b82678
Dmitri Shuralyov <shurcooL@gmail.com>
2017-01-03 17:47:21 -0500

    Improve internal variable naming consistency.

--- elements.go
+++ elements.go
@@ -7,88 +7,88 @@ import (
 
 // H1 returns a h1 element <h1>{{range .nodes}}{{.}}{{end}}</h1>.
 func H1(nodes ...*html.Node) *html.Node {
-	div := &html.Node{
+	h1 := &html.Node{
 		Type: html.ElementNode, Data: atom.H1.String(),
 	}
 	for _, n := range nodes {
-		div.AppendChild(n)
+		h1.AppendChild(n)
 	}
-	return div
+	return h1
 }
 
 // H2 returns a h2 element <h2>{{range .nodes}}{{.}}{{end}}</h2>.
 func H2(nodes ...*html.Node) *html.Node {
-	div := &html.Node{
+	h2 := &html.Node{
 		Type: html.ElementNode, Data: atom.H2.String(),
 	}
 	for _, n := range nodes {
-		div.AppendChild(n)
+		h2.AppendChild(n)
 	}
-	return div
+	return h2
 }
 
 // H3 returns a h3 element <h3>{{range .nodes}}{{.}}{{end}}</h3>.
 func H3(nodes ...*html.Node) *html.Node {
-	div := &html.Node{
+	h3 := &html.Node{
 		Type: html.ElementNode, Data: atom.H3.String(),
 	}
 	for _, n := range nodes {
-		div.AppendChild(n)
+		h3.AppendChild(n)
 	}
-	return div
+	return h3
 }
 
 // H4 returns a h4 element <h4>{{range .nodes}}{{.}}{{end}}</h4>.
 func H4(nodes ...*html.Node) *html.Node {
-	div := &html.Node{
+	h4 := &html.Node{
 		Type: html.ElementNode, Data: atom.H4.String(),
 	}
 	for _, n := range nodes {
-		div.AppendChild(n)
+		h4.AppendChild(n)
 	}
-	return div
+	return h4
 }
 
 // P returns a p element <p>{{range .nodes}}{{.}}{{end}}</p>.
 func P(nodes ...*html.Node) *html.Node {
-	div := &html.Node{
+	p := &html.Node{
 		Type: html.ElementNode, Data: atom.P.String(),
 	}
 	for _, n := range nodes {
-		div.AppendChild(n)
+		p.AppendChild(n)
 	}
-	return div
+	return p
 }
 
 // DL returns a dl element <dl>{{range .nodes}}{{.}}{{end}}</dl>.
 func DL(nodes ...*html.Node) *html.Node {
-	div := &html.Node{
+	dl := &html.Node{
 		Type: html.ElementNode, Data: atom.Dl.String(),
 	}
 	for _, n := range nodes {
-		div.AppendChild(n)
+		dl.AppendChild(n)
 	}
-	return div
+	return dl
 }
 
 // DT returns a dt element <dt>{{range .nodes}}{{.}}{{end}}</dt>.
 func DT(nodes ...*html.Node) *html.Node {
-	div := &html.Node{
+	dt := &html.Node{
 		Type: html.ElementNode, Data: atom.Dt.String(),
 	}
 	for _, n := range nodes {
-		div.AppendChild(n)
+		dt.AppendChild(n)
 	}
-	return div
+	return dt
 }
 
 // DD returns a dd element <dd>{{range .nodes}}{{.}}{{end}}</dd>.
 func DD(nodes ...*html.Node) *html.Node {
-	div := &html.Node{
+	dd := &html.Node{
 		Type: html.ElementNode, Data: atom.Dd.String(),
 	}
 	for _, n := range nodes {
-		div.AppendChild(n)
+		dd.AppendChild(n)
 	}
-	return div
+	return dd
 }
